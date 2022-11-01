a567fcc9a8a0be4dbac0dab6955e16bbe05f16cc
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2022-11-01 14:29:40 +0700

    fix bug sonarqube

--- src/main/java/com/sotatek/authservice/controller/AuthController.java
+++ src/main/java/com/sotatek/authservice/controller/AuthController.java
@@ -11,13 +11,11 @@ import com.sotatek.authservice.service.AuthenticationService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
