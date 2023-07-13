78c0da433b1763cd2403c2b6b9790d21edff090c
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2023-07-13 09:51:49 +0700

    fix: set profile for repository test

--- src/test/java/org/cardanofoundation/authentication/repository/RepositoryTest.java
+++ src/test/java/org/cardanofoundation/authentication/repository/RepositoryTest.java
@@ -21,11 +21,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @TestPropertySource(locations = "classpath:application.properties")
+@ActiveProfiles("dev")
 class RepositoryTest {
 
   @Autowired
