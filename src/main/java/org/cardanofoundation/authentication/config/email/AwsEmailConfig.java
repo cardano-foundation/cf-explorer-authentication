3c594a987835414982067e9b18089ca325dc7f04
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2023-07-25 15:11:08 +0100

    feat: adde SES Configuration To send emails

---
+++ src/main/java/org/cardanofoundation/authentication/config/email/AwsEmailConfig.java
@@ -0,0 +1,50 @@
+package org.cardanofoundation.authentication.config.email;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.mail.simplemail.SimpleEmailServiceJavaMailSender;
+import org.springframework.cloud.aws.mail.simplemail.SimpleEmailServiceMailSender;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Primary
+@Profile("aws")
+@Configuration
+public class AwsEmailConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+
+    @Bean
+    public MailSender mailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
+        return new SimpleEmailServiceMailSender(amazonSimpleEmailService);
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
+        return new SimpleEmailServiceJavaMailSender(amazonSimpleEmailService);
+    }
+
+}
